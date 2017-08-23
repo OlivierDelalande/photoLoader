b811e1de0d02d6f8fc96fad9fe87d3a1799dffc0
olivierbranch <delalande.o@g.com>
2017-08-23 16:28:08 +0200

    clean up

--- photoLoader/src/app/photo-loader/photo-loader.component.ts
+++ photoLoader/src/app/photo-loader/photo-loader.component.ts
@@ -3,15 +3,13 @@ import {
   OnInit
 } from '@angular/core';
 import {PhotoLoaderService} from '../services/photo-loader.service';
-// import {FileUploader} from 'ng2-file-upload';
 import {
   FormBuilder,
   FormGroup,
   FormControl
 } from '@angular/forms';
 import {
-  Http,
-  RequestOptions,
+  Http
 } from '@angular/http';
 import {Observable} from 'rxjs/Observable';
 
@@ -27,10 +25,7 @@ export class PhotoLoaderComponent implements OnInit {
     test2: "world"
       };
   filesToUpload: Array<File>;
-  jsonPicture;
-  picture;
   loadPictureForm: FormGroup;
-  // public uploader: FileUploader = new FileUploader({url: ''});
 
   constructor(
     private photoLoader: PhotoLoaderService,
@@ -97,66 +92,29 @@ export class PhotoLoaderComponent implements OnInit {
 
 
   fileChangeEvent(event) {
-    let fileList: FileList = event.target.files;
-    if (fileList.length > 0) {
-      console.log('filelist',fileList[0]);
-      let file: File = fileList[0];
-      console.log('file', file);
-      let formData: FormData = new FormData();
-      formData.append('uploadFile', file, file.name);
-      console.log('formdata1', formData.get('uploadFile'));
-      let headers = new Headers();
-      /** No need to include Content-Type in Angular 4 */
-      headers.append('Content-Type', 'multipart/form-data');
-      headers.append('Accept', 'application/json');
-      // let options = new RequestOptions({headers: headers});
-      console.log('formdata2', formData.get('uploadFile'));
-      this.http.post('https://us-central1-photo-loader.cloudfunctions.net/api/upload/', formData)
-          .map(res => res.json())
-          .catch(error => Observable.throw(error))
-          .subscribe(
-            data => console.log('success', data),
-            error => console.log(error)
-          )
+    if(event && event.target){
+      let fileList: FileList = event.target.files;
+      if (fileList.length > 0) {
+        // console.log('filelist',fileList[0]);
+        let file: File = fileList[0];
+        // console.log('file', file);
+        let formData: FormData = new FormData();
+        formData.append('uploadFile', file, file.name);
+        // console.log('formdata1', formData.get('uploadFile'));
+        let headers = new Headers();
+        /** No need to include Content-Type in Angular 4 */
+        headers.append('Content-Type', 'multipart/form-data');
+        headers.append('Accept', 'application/json');
+        // let options = new RequestOptions({headers: headers});
+        console.log('formdata2', formData.get('uploadFile'));
+        this.http.post('https://us-central1-photo-loader.cloudfunctions.net/api/upload/', formData)
+            .map(res => res.json())
+            .catch(error => Observable.throw(error))
+            .subscribe(
+              data => console.log('success', data),
+              error => console.log(error)
+            )
+      }
     }
   }
-
-
-
-
-
-
-  //   const eventObj: MSInputMethodContext = event as MSInputMethodContext;
-  //   const target: HTMLInputElement = eventObj.target as HTMLInputElement;
-  //   const files: FileList = target.files;
-  //
-  //   if (files && files[0]) {
-  //
-  //     const reader = new FileReader();
-  //
-  //     reader.onload = (nevent: any) => {
-  //       this.picture = nevent.target.result;
-  //     };
-  //     reader.readAsDataURL(event.target.files[0]);
-  //
-  //     this._file = files[0];
-  //     this.fileName = this._file.name;
-  //   } else {
-  //     this._file = null;
-  //     this.fileName = null;
-  //   }
-  //   this.propagateChange(this._file);
-  // }
-
-
-  // convert(img) {
-  //   var canvas = document.createElement("canvas");
-  //   canvas.width = img.width;
-  //   canvas.height = img.height;
-  //   var ctx = canvas.getContext("2d");
-  //   ctx.drawImage(img, 0, 0);
-  //   var dataURL = canvas.toDataURL("image/png");
-  //   return dataURL.replace(/^data:image\/(png|jpg);base64,/, "");
-  // }
-
 }
